91143668a07f999e4a5a92eaaa5ed1b0d47a6b88
coder_ <satvikbhatnagar420@gmail.com>
2024-07-01 19:58:08 +0000

    init
    Signed-off-by: coder_ <satvikbhatnagar420@gmail.com>

---
+++ banks_project.py
@@ -0,0 +1,9 @@
+url_data = 'https://web.archive.org/web/20230908091635 /https://en.wikipedia.org/wiki/List_of_largest_banks'
+csv_exchange_rate = './exchange_rate.csv'
+output_csv_path = './Largest_banks_data.csv'
+log_file = './code_log.txt'
+target_file = "./Banks.db"
+
+table_att_initial = ['Name', 'MC_USD_Billion']
+table_att_final = ['Name', 'MC_USD_Billion', 'MC_GBP_Billion', 'MC_EUR_Billion', 'MC_INR_Billion']
+table_name = 'Largest_banks'
