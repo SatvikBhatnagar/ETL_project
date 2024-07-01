d2ed351383073a9eb7a7a49221aa8a0c412f57c1
coder_ <satvikbhatnagar420@gmail.com>
2024-07-01 20:28:07 +0000

    add extract function
    Signed-off-by: coder_ <satvikbhatnagar420@gmail.com>

--- banks_project.py
+++ banks_project.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
 
 url_data = 'https://web.archive.org/web/20230908091635 /https://en.wikipedia.org/wiki/List_of_largest_banks'
 csv_exchange_rate = './exchange_rate.csv'
@@ -19,3 +22,30 @@ def log_progress(message):
     timestamp = now.strftime(timestamp_format)
     with open(log_file, 'a') as f:
         f.write(timestamp + ',' + message + '\n')
+
+
+def extract(url, table_att):
+    """ The purpose of this function is to extract the required
+        information from the website and save it to a dataframe.
+        The function returns the dataframe for further processing. """
+    df_ = pd.DataFrame(columns=table_att)
+    html_page = requests.get(url).text
+    data = BeautifulSoup(html_page, 'html.parser')
+    tables = data.find_all('tbody')
+    rows = tables[0].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        if len(col) != 0:
+            data_dict = {"Name": col[1].text.strip(),
+                         "MC_USD_Billion": col[2].text.strip(), }
+            df1 = pd.DataFrame(data_dict, index=[0])
+            df_ = pd.concat([df_, df1], ignore_index=True)
+
+    return df_
+
+
+log_progress("Preliminaries complete. Initializing ETL process")
+
+df = extract(url_data, table_att_initial)
+log_progress("Data extraction complete. Initializing Transformation process")
+print(df)
