ea686d023488e052ac7c745f6add87cba3237d35
coder_ <satvikbhatnagar420@gmail.com>
2024-07-01 20:02:15 +0000

    create log_progress function
    Signed-off-by: coder_ <satvikbhatnagar420@gmail.com>

--- banks_project.py
+++ banks_project.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 url_data = 'https://web.archive.org/web/20230908091635 /https://en.wikipedia.org/wiki/List_of_largest_banks'
 csv_exchange_rate = './exchange_rate.csv'
 output_csv_path = './Largest_banks_data.csv'
@@ -7,3 +9,13 @@ target_file = "./Banks.db"
 table_att_initial = ['Name', 'MC_USD_Billion']
 table_att_final = ['Name', 'MC_USD_Billion', 'MC_GBP_Billion', 'MC_EUR_Billion', 'MC_INR_Billion']
 table_name = 'Largest_banks'
+
+
+def log_progress(message):
+    """ This function logs the mentioned message at a given stage of the code execution to a log file.
+    Function returns nothing"""
+    timestamp_format = '%Y-%h-%d %H:%M:%S'
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format)
+    with open(log_file, 'a') as f:
+        f.write(timestamp + ',' + message + '\n')
