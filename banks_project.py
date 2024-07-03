de355bc3ba91afff036c2cc2db5d27156b8c507c
coder_ <satvikbhatnagar420@gmail.com>
2024-07-03 09:14:23 +0000

    update code to score 10.00/10 Pylint rating

--- banks_project.py
+++ banks_project.py
@@ -1,29 +1,32 @@
+"""Complete ETL process"""
+
+import sqlite3
 from datetime import datetime
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-import sqlite3
 
-url_data = 'https://web.archive.org/web/20230908091635 /https://en.wikipedia.org/wiki/List_of_largest_banks'
-csv_exchange_rate = './exchange_rate.csv'
-output_csv_path = './Largest_banks_data.csv'
-log_file = './code_log.txt'
-target_file = "./Banks.db"
+URL_DATA = ('https://web.archive.org/web/20230908091635'
+            '/https://en.wikipedia.org/wiki/List_of_largest_banks')
+CSV_EXCHANGE_RATE = './exchange_rate.csv'
+OUTPUT_CSV_PATH = './Largest_banks_data.csv'
+LOG_FILE = './code_log.txt'
+TARGET_FILE = "./Banks.db"
 
 table_att_initial = ['Name', 'MC_USD_Billion']
 table_att_final = ['Name', 'MC_USD_Billion', 'MC_GBP_Billion', 'MC_EUR_Billion', 'MC_INR_Billion']
-table_name = 'Largest_banks'
+TABLE_NAME = 'Largest_banks'
 
-conn = sqlite3.connect(target_file)
+conn = sqlite3.connect(TARGET_FILE)
 
 
 def log_progress(message):
-    """ This function logs the mentioned message at a given stage of the code execution to a log file.
-    Function returns nothing"""
+    """ This function logs the mentioned message at a given stage of the code execution to a
+    log file. Function returns nothing"""
     timestamp_format = '%Y-%h-%d %H:%M:%S'
     now = datetime.now()
     timestamp = now.strftime(timestamp_format)
-    with open(log_file, 'a') as f:
+    with open(LOG_FILE, 'a', encoding='utf-8') as f:
         f.write(timestamp + ',' + message + '\n')
 
 
@@ -32,7 +35,7 @@ def extract(url, table_att):
         information from the website and save it to a dataframe.
         The function returns the dataframe for further processing. """
     df_ = pd.DataFrame(columns=table_att)
-    html_page = requests.get(url).text
+    html_page = requests.get(url, timeout=10).text
     data = BeautifulSoup(html_page, 'html.parser')
     tables = data.find_all('tbody')
     rows = tables[0].find_all('tr')
@@ -52,14 +55,13 @@ def transform(df_):
         format to float value, transforms the information of GDP from
         USD (Millions) to USD (Billions) rounding to 2 decimal places.
         The function returns the transformed dataframe."""
-    # df_ = df_.index
     try:
-        exchange_rates = pd.read_csv(csv_exchange_rate)
+        exchange_rates = pd.read_csv(CSV_EXCHANGE_RATE)
         count = 0
         for rate in exchange_rates.iloc[:, 1]:
-            for attr in exchange_rates.iloc[:,0]:
-                if attr in table_att_final[count+2].split('_'):
-                    df_[table_att_final[count+2]] = round(rate * df_.iloc[:,1],2)
+            for attr in exchange_rates.iloc[:, 0]:
+                if attr in table_att_final[count + 2].split('_'):
+                    df_[table_att_final[count + 2]] = round(rate * df_.iloc[:, 1], 2)
             count += 1
     except FileNotFoundError:
         print('csv file not found')
@@ -76,7 +78,7 @@ def load_to_csv(df_, file_path):
 def load_to_db(df_):
     """ This function saves the final data frame to a database
         table with the provided name. Function returns nothing."""
-    df_.to_sql(table_name, con=conn, if_exists='replace')
+    df_.to_sql(TABLE_NAME, con=conn, if_exists='replace')
 
 
 def run_query(query_statement, conn_):
@@ -89,31 +91,31 @@ def run_query(query_statement, conn_):
 
 log_progress("Preliminaries complete. Initializing ETL process")
 
-df = extract(url_data, table_att_initial)
+df = extract(URL_DATA, table_att_initial)
 log_progress("Data extraction complete. Initializing Transformation process")
 
 df = transform(df)
 log_progress("Transformation complete. Saving the df to the csv file")
 
-load_to_csv(df, output_csv_path)
+load_to_csv(df, OUTPUT_CSV_PATH)
 log_progress("Data saved to the csv file. Saving the df to the DB")
 
 load_to_db(df)
 log_progress("Data saved to the DB. Running some queries")
 
-query_statement1 = f'SELECT * FROM {table_name}'
-query_statement2 = f'SELECT * FROM {table_name} WHERE MC_USD_Billion > 200'
-query_statement3 = f'SELECT * FROM Largest_banks'
-query_statement4 = f'SELECT AVG(MC_GBP_Billion) FROM {table_name}'
-query_statement5 = f'SELECT Name from Largest_banks LIMIT 5'
+query_statement1 = f'SELECT * FROM {TABLE_NAME}'
+query_statement2 = f'SELECT * FROM {TABLE_NAME} WHERE MC_USD_Billion > 200'
+QUERY_STATEMENT3 = 'SELECT * FROM Largest_banks'
+query_statement4 = f'SELECT AVG(MC_GBP_Billion) FROM {TABLE_NAME}'
+QUERY_STATEMENT5 = 'SELECT Name from Largest_banks LIMIT 5'
 
 try:
     run_query(query_statement1, conn)
     run_query(query_statement2, conn)
-    run_query(query_statement3, conn)
+    run_query(QUERY_STATEMENT3, conn)
     run_query(query_statement4, conn)
-    run_query(query_statement5, conn)
-except Exception as e:
+    run_query(QUERY_STATEMENT5, conn)
+except (sqlite3.OperationalError, sqlite3.IntegrityError, sqlite3.DatabaseError) as e:
     print('Exception when executing run_query():', e)
 log_progress("Queries executed successfully")
 
